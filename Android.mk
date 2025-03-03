66e2a493533d5a655e894365bc825ffd898ff413
resparkstar <39150614+resparkstar@users.noreply.github.com>
2025-03-03 10:58:20 +0800

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for samsung SM-J7109 (j7xltectc)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),j7xltectc)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for samsung SM-J7109 (j7xltectc)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_j7xltectc-user
+add_lunch_combo omni_j7xltectc-userdebug
+add_lunch_combo omni_j7xltectc-eng
